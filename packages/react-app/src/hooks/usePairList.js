72d46c5a78328cae2f6f183cb33fc6c595283f38
realcee <frasier@hotmail.fr>
2022-08-29 15:12:28 +0200

    add protection of calling usepairList hook

--- packages/react-app/src/hooks/usePairList.js
+++ packages/react-app/src/hooks/usePairList.js
@@ -34,14 +34,16 @@ export default function usePairList(graphQlUri) {
     // const { loading, data } = useQuery(EXAMPLE_GQL, { pollInterval: 2500 });
 
     useEffect ( ()=> {
-        console.log("suuposed grahql uri :",graphQlUri)
+        console.log("suposed grahql uri :",graphQlUri)
+        if(graphQlUri) {
         fetch(graphQlUri, {
             method: "post",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({query : EXAMPLE_GRAPHQL}),
           }).then(response => response.json())
             .then(data => setPairs(data.data.pairs))
-            .catch (e => console.log("Erreur Sa mere ",e));
+            .catch (e => console.log("Erreur Sa mere ",e));}
+            else {setPairs({})}
      
     },[graphQlUri])
 
